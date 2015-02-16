1f5b28d05c4cc95969bd1a133d41dfd2887a4019
JustSteveKing <juststeveking@gmail.com>
2015-02-16 15:28:24 +0000

    Adding Routes to index file

--- api/index.php
+++ api/index.php
@@ -22,3 +22,47 @@ $app->configureMode('development', function () use($app) {
 		))
 	);
 });
+
+$app->get('/', function () use($app) {
+	$app->redirect('/v1', 301);
+});
+
+$app->group('/v1', function () use($app) {
+	
+	$app->contentType('application/json');
+
+	$app->group('/posts', function () use($app) {
+		$app->get('/', function () use($app) {
+			// Get all of the posts from the Model
+		});
+		$app->get('/:id', function () use($app) {
+			// Get a single Post
+		})
+	});
+
+	$app->group('/categories', function () use($app) {
+		$app->get('/', function () use($app) {
+			// Get all of the Categories
+		});
+		$app->get('/:id', function () use($app) {
+			// Get a single Category
+		});
+	});
+
+	$app->group('/authors', function () use($app) {
+		$app->get('/', function () use($app) {
+			// Get all Authors
+		});
+		$app->get('/:id', function () use($app) {
+			// Get a single Author
+		})
+	});
+
+});
+
+$app->notfound(function () use($app) {
+	echo "404 Page"''
+});
+
+/* AND DEPLOY */
+$app->run();
