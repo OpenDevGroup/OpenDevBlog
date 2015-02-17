8b2f3740eac316f4c51ba3e4e15b0b0ee6495c2b
JustSteveKing <juststeveking@gmail.com>
2015-02-17 11:57:17 +0000

    Trimmed out none required or too heavy methods

--- api/src/Util.php
+++ api/src/Util.php
@@ -10,19 +10,23 @@
 namespace OpenDev;
 
 class Util {
-    
-    const SECONDS_IN_A_MINUTE = 60;
-    const SECONDS_IN_A_HOUR = 3600;
-    const SECONDS_IN_AN_HOUR = 3600;
-    const SECONDS_IN_A_DAY = 86400;
-    const SECONDS_IN_A_WEEK = 604800;
-    const SECONDS_IN_A_MONTH = 2592000;
-    const SECONDS_IN_A_YEAR = 31536000;
-   
+
+    /**
+     * Counts the amount of words in a given string
+     *
+     * @param   string  $str   A string to count the words within it
+     * @return  int
+     */
     public static function wordCount($str) {
         return count(preg_split('/\s+/', strip_tags($str), null, PREG_SPLIT_NO_EMPTY));
     }
 
+    /**
+     * Calculate the time it would take to read a string.
+     *
+     * @param   string  $content   A string that we want to calculate reading time on
+     * @return  string
+     */
     public static function readingTime($content) {
         $word = self::wordCount($content);
         $min = floor($word / 175);
@@ -34,6 +38,14 @@ class Util {
         }
     }
 
+    /**
+     * Slugify a string passed for use in URI's and CSS mode
+     *
+     * @param   string  $string   A string to convert to a slug
+     * @param   boolean $css_mode Whether or not to generate strings safe for
+     *                            CSS classes/IDs (Default to false)
+     * @return  string
+     */
     public static function slugify($string, $css_mode = FALSE) {
         $slug = preg_replace('/([^a-z0-9]+)/', '-', strtolower(self::remove_accents($string)));
         if ($css_mode) {
@@ -44,251 +56,6 @@ class Util {
             }
         }
         return $slug;
-    }
-            
-    public static function humanTimeDiff($from, $to = '', $as_text = FALSE, $suffix = ' ago') {
-        if ($to == '') {
-            $to = time();
-        }
-        
-        $from = new \DateTime(date('Y-m-d H:i:s', $from));
-        $to   = new \DateTime(date('Y-m-d H:i:s', $to));
-        $diff = $from->diff($to);
-        
-        if ($diff->y > 1) {
-            $text = $diff->y . ' years';
-        } else if ($diff->y == 1) {
-            $text = '1 year';
-        } else if ($diff->m > 1) {
-            $text = $diff->m . ' months';
-        } else if ($diff->m == 1) {
-            $text = '1 month';
-        } else if ($diff->d > 7) {
-            $text = ceil($diff->d / 7) . ' weeks';
-        } else if ($diff->d == 7) {
-            $text = '1 week';
-        } else if ($diff->d > 1) {
-            $text = $diff->d . ' days';
-        } else if ($diff->d == 1) {
-            $text = '1 day';
-        } else if ($diff->h > 1) {
-            $text = $diff->h . ' hours';
-        } else if ($diff->h == 1) {
-            $text = ' 1 hour';
-        } else if ($diff->i > 1) {
-            $text = $diff->i . ' minutes';
-        } else if ($diff->i == 1) {
-            $text = '1 minute';
-        } else if ($diff->s > 1) {
-            $text = $diff->s . ' seconds';
-        } else {
-            $text = '1 second';
-        }
-        
-        if ($as_text) {
-            $text = explode(' ', $text, 2);
-            $text = self::numberToWord($text[0]) . ' ' . $text[1];
-        }
-        
-        return trim($text) . $suffix;
-    }
-    
-    public static function numberToWord($number) {
-        $number = (string) $number;
-        
-        if (strpos($number, '.') !== FALSE) {
-            list($number, $decimal) = explode('.', $number);
-        } else {
-            $number  = $number;
-            $decimal = FALSE;
-        }
-        
-        $output = '';
-        
-        if ($number[0] == '-') {
-            $output = 'negative ';
-            $number = ltrim($number, '-');
-        } else if ($number[0] == '+') {
-            $output = 'positive ';
-            $number = ltrim($number, '+');
-        }
-        
-        if ($number[0] == '0') {
-            $output .= 'zero';
-        } else {
-            $number = str_pad($number, 36, '0', STR_PAD_LEFT);
-            $group  = rtrim(chunk_split($number, 3, ' '), ' ');
-            $groups = explode(' ', $group);
-            
-            $groups2 = array();
-            
-            foreach ($groups as $group) {
-                $groups2[] = self::numberToWordThreeDigit($group[0], $group[1], $group[2]);
-            }
-            
-            for ($z = 0; $z < count($groups2); $z++) {
-                if ($groups2[$z] != '') {
-                    $output .= $groups2[$z] . self::numberToWordConvertGroup(11 - $z);
-                    $output .= ($z < 11 && !array_search('', array_slice($groups2, $z + 1, -1)) && $groups2[11] != '' && $groups[11][0] == '0' ? ' and ' : ', ');
-                }
-            }
-            
-            $output = rtrim($output, ', ');
-        }
-        
-        if ($decimal > 0) {
-            $output .= ' point';
-            
-            for ($i = 0; $i < strlen($decimal); $i++) {
-                $output .= ' ' . self::numberToWordConvertDigit($decimal[$i]);
-            }
-        }
-        
-        return $output;
-    }
-    
-    protected static function numberToWordConvertGroup($index) {
-        switch ($index) {
-            case 11:
-                return ' decillion';
-            case 10:
-                return ' nonillion';
-            case 9:
-                return ' octillion';
-            case 8:
-                return ' septillion';
-            case 7:
-                return ' sextillion';
-            case 6:
-                return ' quintrillion';
-            case 5:
-                return ' quadrillion';
-            case 4:
-                return ' trillion';
-            case 3:
-                return ' billion';
-            case 2:
-                return ' million';
-            case 1:
-                return ' thousand';
-            case 0:
-                return '';
-        }
-    }
-    
-    protected static function numberToWordThreeDigit($digit1, $digit2, $digit3) {
-        $output = '';
-        
-        if ($digit1 == '0' && $digit2 == '0' && $digit3 == '0') {
-            return '';
-        }
-        
-        if ($digit1 != '0') {
-            $output .= self::numberToWordConvertDigit($digit1) . ' hundred';
-            
-            if ($digit2 != '0' || $digit3 != '0') {
-                $output .= ' and ';
-            }
-        }
-        if ($digit2 != '0') {
-            $output .= self::numberToWordTwoDigit($digit2, $digit3);
-        } else if ($digit3 != '0') {
-            $output .= self::numberToWordConvertDigit($digit3);
-        }
-        
-        return $output;
-    }
-    
-    protected static function numberToWordTwoDigit($digit1, $digit2) {
-        if ($digit2 == '0') {
-            switch ($digit1) {
-                case '1':
-                    return 'ten';
-                case '2':
-                    return 'twenty';
-                case '3':
-                    return 'thirty';
-                case '4':
-                    return 'forty';
-                case '5':
-                    return 'fifty';
-                case '6':
-                    return 'sixty';
-                case '7':
-                    return 'seventy';
-                case '8':
-                    return 'eighty';
-                case '9':
-                    return 'ninety';
-            }
-        } else if ($digit1 == '1') {
-            switch ($digit2) {
-                case '1':
-                    return 'eleven';
-                case '2':
-                    return 'twelve';
-                case '3':
-                    return 'thirteen';
-                case '4':
-                    return 'fourteen';
-                case '5':
-                    return 'fifteen';
-                case '6':
-                    return 'sixteen';
-                case '7':
-                    return 'seventeen';
-                case '8':
-                    return 'eighteen';
-                case '9':
-                    return 'nineteen';
-            }
-        } else {
-            $second_digit = self::numberToWordConvertDigit($digit2);
-            
-            switch ($digit1) {
-                case '2':
-                    return "twenty-{$second_digit}";
-                case '3':
-                    return "thirty-{$second_digit}";
-                case '4':
-                    return "forty-{$second_digit}";
-                case '5':
-                    return "fifty-{$second_digit}";
-                case '6':
-                    return "sixty-{$second_digit}";
-                case '7':
-                    return "seventy-{$second_digit}";
-                case '8':
-                    return "eighty-{$second_digit}";
-                case '9':
-                    return "ninety-{$second_digit}";
-            }
-        }
-    }
-    
-    protected static function numberToWordConvertDigit($digit) {
-        switch ($digit) {
-            case '0':
-                return 'zero';
-            case '1':
-                return 'one';
-            case '2':
-                return 'two';
-            case '3':
-                return 'three';
-            case '4':
-                return 'four';
-            case '5':
-                return 'five';
-            case '6':
-                return 'six';
-            case '7':
-                return 'seven';
-            case '8':
-                return 'eight';
-            case '9':
-                return 'nine';
-        }
     }
 
     /**
